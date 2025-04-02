0250ef831e7f528ac3a34a36f22cdd2144d90dba
JeveCavalheiro <jeveedev@gmail.com>
2025-04-01 21:10:30 -0300

    implementada a conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 import express from "express";
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize('biblioteca2025', 'postgres', 'JevePostgres', {
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres', 
+    define: {
+        timestamps: false,
+        freezeTableName: true
+    }
+  });
+
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão com o banco de dados realizada com sucesso.');
+  } catch (error) {
+    console.error('Erro ao conectar com o banco de dados:', error);
+  }
 
 const app = express();
 app.use(express.json());
